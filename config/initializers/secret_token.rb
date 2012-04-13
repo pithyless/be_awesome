3de5162634ad0fc17316dbf7d5824c2d0fc1782a
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-04-13 15:40:40 +0200

    Initialize Awesomeness!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeAwesome::Application.config.secret_token = 'ab5ed41949e2fa7fb1274502701219407743980cd5ea773011da2fd5fc756aad565a31e7c22fc44ccaf9996216fdbbf7277ecda6d9d8264ccbbb6d92c33f7e7f'
