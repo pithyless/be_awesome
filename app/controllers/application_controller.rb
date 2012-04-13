c28c9f17372b5727ee1066b169f034b80343e30d
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-04-13 23:47:55 +0200

    Force login for all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :check_sign_in
+
   helper_method :current_user
 
   private
+
+  def check_sign_in
+    unless current_user
+    redirect_to '/auth/facebook'
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
 
+  skip_before_filter :check_sign_in, :only => [:create]
+
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_facebook_uid(auth["uid"]) || User.create_with_facebook(auth)
