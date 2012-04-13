872c440a671933df9994a279e5fb43cbfda3232f
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-04-14 00:27:32 +0200

    Make project development ENV friendly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ class ApplicationController < ActionController::Base
   private
 
   def check_sign_in
+    if Rails.env.development?
+      user = User.new.tap do |u|
+        u.id = '424242'
+        u.name = 'Billy Joel'
+        u.email = 'billy@joel.com'
+        u.avatar = 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/573520_1180408451_1360557398_q.jpg'
+      end
+      session[:user_id] = '424242'
+      @current_user = user
+    end
+
     unless current_user
       redirect_to '/auth/facebook'
     end
--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,6 @@
-
-DB = Mongo::Connection.new(ENV['AWESOME_MONGO_HOST'], ENV['AWESOME_MONGO_PORT']).db('beawesome')
-DB.authenticate(ENV['AWESOME_MONGO_USER'], ENV['AWESOME_MONGO_SECRET'])
-
-# TODO: add indexes
+if Rails.env.production?
+  DB = Mongo::Connection.new(ENV['AWESOME_MONGO_HOST'], ENV['AWESOME_MONGO_PORT']).db('beawesome')
+  DB.authenticate(ENV['AWESOME_MONGO_USER'], ENV['AWESOME_MONGO_SECRET'])
+else
+  DB = Mongo::Connection.new('localhost').db('beawesome')
+end
