b367b9876375912ed22549008dfbb980e499d1e7
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-04-13 22:47:42 +0200

    Add login capability

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    User.find_by_facebook_uid(auth["uid"]) || User.create_with_facebook(auth)
+    user = User.find_by_facebook_uid(auth["uid"]) || User.create_with_facebook(auth)
     session[:user_id] = user.id
     redirect_to root_url
   end
